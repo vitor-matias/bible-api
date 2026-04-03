ba9fb75ac78d8b03136e76dfc305222428e88f14
Vítor Matias <vitor.matias@pm.me>
2026-04-03 20:07:32 +0100

    chore: disable embedding generation and remove docker-compose version field

--- src/services/usfmImportService/storeChapter.ts
+++ src/services/usfmImportService/storeChapter.ts
@@ -157,7 +157,7 @@ export const storeChapter = async (
   }
 
   // Generate embeddings in batch for all verses in the chapter
-  if (versesData.length > 0) {
+  if (false) {
     const texts = versesData.map((v) => v.text)
 
     console.log(
